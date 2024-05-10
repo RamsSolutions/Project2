3e5d1274306544ea1f7d65bbe315d12c2f307b2f
primo <ramatlharesurprise@gmail.com>
2024-05-10 09:07:05 +0200

    CODE ADDITION

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your name");
+        String name = input.next();
+        System.out.println("Hello " + name + " Welcome to our first code");
     }
 }
